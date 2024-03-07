b06014602d1318b295f8668635c5b03bb7757793
oc-garcia <octaviogarcia1988@gmail.com>
2024-03-07 12:29:52 -0300

    Add file reading functionality and update package.json

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-console.log('olá mundo');
+import fs from "fs";
+import chalk from "chalk";
+
+const getArchive = (path) => {
+  const encoding = "utf-8";
+  fs.readFile(path, encoding, (err, data) => {
+    if (err) {
+      console.error(chalk.red(err));
+      return;
+    }
+    console.log(chalk.green(data));
+  });
+};
+
+getArchive("./arquivos/texto.md");
