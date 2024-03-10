67cd4bf28ba7f21abbef92e145e222dad72cf47c
oc-garcia <octaviogarcia1988@gmail.com>
2024-03-10 15:29:27 -0300

    Refactor getArchive function to use async/await

--- index.js
+++ index.js
@@ -5,22 +5,14 @@ const handleError = (err) => {
   throw new Error(chalk.red(err.code));
 };
 
-const getArchive = (path) => {
+const getArchive = async (path) => {
   const encoding = "utf-8";
-  fs.promises
-    .readFile(path, encoding)
-    .then((data) => console.log(chalk.green(data)))
-    .catch(handleError);
+  try {
+    const data = await fs.promises.readFile(path, encoding);
+    console.log(chalk.green(data));
+  } catch (error) {
+    handleError(error);
+  }
 };
-
-// const getArchive = async (path) => {
-//   const encoding = "utf-8";
-//   await fs.readFile(path, encoding, (err, data) => {
-//     if (err) {
-//       handleError(err);
-//     }
-//     console.log(chalk.green(data));
-//   });
-// };
 
 getArchive("./archives/text.md");
